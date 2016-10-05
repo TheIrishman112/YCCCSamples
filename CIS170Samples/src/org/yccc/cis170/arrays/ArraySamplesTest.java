ee4bc519f68984bd4cb75cf956b4c24d8c7638b5
Michael Bourgeois <mike.bourgeois@gmail.com>
2016-10-05 09:24:15 -0400

    Update org

---
+++ CIS170Samples/src/org/yccc/cis170/arrays/ArraySamplesTest.java
@@ -0,0 +1,14 @@
+package org.yccc.cis170.arrays;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArraySamplesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ CIS170Samples/src/org/yccc/cis170/io/IOSamples.java
@@ -0,0 +1,10 @@
+package org.yccc.cis170.io;
+
+public class IOSamples {
+	
+	private void collectUserInput()
+	{
+		
+	}
+
+}
