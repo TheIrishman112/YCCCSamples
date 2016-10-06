036130110aac4bb3d6ba11f4e819438b52a93377
mbourgeois <mbourgeois@C218-1750.yccc.edu>
2016-10-06 19:47:58 -0400

    Cleaning up assignment

--- CIS170Samples/src/org/yccc/cis170/arrays/ArrayAssignment.java
+++ CIS170Samples/src/org/yccc/cis170/arrays/ArrayAssignment.java
@@ -1,21 +1,19 @@
 package org.yccc.cis170.arrays;
 
 public class ArrayAssignment {
-	
-	// Add code to remove duplicates from an array.
-	public String[] removeDupes(String[] listOfStrings)
-	{
-		String[] dupeFree = null;
-		
-		return dupeFree;
-	}
-	
+
 	// Add code to test if an array has the string value passed.
-	public boolean containsValue(String[] stringArray, String testValue)
-	{
+	public boolean containsValue(String[] stringArray, String testValue) {
 		boolean containsValue = false;
 		// Your code here...
 		return containsValue;
+	}
+
+	// Add code to remove duplicates from an array.
+	public String[] removeDupes(String[] listOfStrings) {
+		String[] dupeFree = null;
+		// your code here
+		return dupeFree;
 	}
 
 }
