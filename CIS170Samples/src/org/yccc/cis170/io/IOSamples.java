2315000e7a6bd60c2a36a26f6784bd1bc743ddf0
Michael Bourgeois <mike.bourgeois@gmail.com>
2016-10-06 16:23:02 -0400

    Adding IO Samples

--- CIS170Samples/src/org/yccc/cis170/io/IOSamples.java
+++ CIS170Samples/src/org/yccc/cis170/io/IOSamples.java
@@ -1,10 +1,49 @@
 package org.yccc.cis170.io;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class IOSamples {
-	
-	private void collectUserInput()
-	{
-		
+
+	Scanner console = new Scanner(System.in);
+
+	// Interacting on the console.
+	public void smallTalk() {
+		System.out.println("Hi, what's your name? ");
+		String name = console.next();
+		System.out.println("Thanks " + name + ", how old are you?");
+		int age = console.nextInt();
+		System.out.println("Wow, " + name + " you look great for " + age + "!");
+	}
+
+	// Reading from a file
+	public void readFile() {
+		try {
+			Scanner inFile = new Scanner(new FileReader("data.txt"));
+
+			while (inFile.hasNext()) {
+				System.out.println(inFile.next());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// File Writing sample
+	public void writeFile() {
+		BufferedWriter out = null;
+		try {
+			FileWriter fstream = new FileWriter("out.txt");
+			out = new BufferedWriter(fstream);
+			out.write("Hello Java, ");
+			out.write("how have you been?");
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
