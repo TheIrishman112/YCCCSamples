e56e65e1ab8af93fb0226c19168c2a82e5cc76d9
mbourgeois <mbourgeois@C218-1750.yccc.edu>
2016-10-06 19:39:59 -0400

    Updating array assignment and tests.

--- CIS170Samples/src/org/yccc/cis170/arrays/ArrayAssignment.java
+++ CIS170Samples/src/org/yccc/cis170/arrays/ArrayAssignment.java
@@ -2,11 +2,20 @@ package org.yccc.cis170.arrays;
 
 public class ArrayAssignment {
 	
+	// Add code to remove duplicates from an array.
 	public String[] removeDupes(String[] listOfStrings)
 	{
 		String[] dupeFree = null;
 		
 		return dupeFree;
+	}
+	
+	// Add code to test if an array has the string value passed.
+	public boolean containsValue(String[] stringArray, String testValue)
+	{
+		boolean containsValue = false;
+		// Your code here...
+		return containsValue;
 	}
 
 }
--- CIS170Samples/src/org/yccc/cis170/arrays/ArraySamplesTest.java
+++ CIS170Samples/src/org/yccc/cis170/arrays/ArraySamplesTest.java
@@ -5,21 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ArraySamplesTest {
-
+	String[] withDupes = new String[]{"dog", "cat", "dog", "giraffe"};
+	
 	@Test
-	public void test() {
+	public void testRemoveDupes() {
 		ArrayAssignment aa = new ArrayAssignment();
-		
-		String[] withDupes = new String[]{"dog", "cat", "dog", "giraffe"};
+	
 		String[] dupeFree = aa.removeDupes(withDupes);
-		
+		int dogCount = 0;
 		for(int i = 0; i < dupeFree.length; i++)
 		{
 			if("dog".equals(dupeFree[i]))
 			{
-				fail("Still has dupes!");
+				dogCount += 1;
 			}
 		}
+		
+		if(dogCount > 1)
+		{
+			fail("Too many dogs!");
+		}
 	}
-
+	
+	@Test
+	public void testContains()
+	{
+		ArrayAssignment aa = new ArrayAssignment();
+		assertTrue(aa.containsValue(withDupes, "dog"));
+		assertFalse(aa.containsValue(withDupes, "super"));
+	}
 }
